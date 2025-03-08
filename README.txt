2201acbc24391be1e2645a84cba90001c131f370
worldconflictmap <parkdongsu0718@gmail.com>
2025-03-09 07:47:43 +0900

    add all

---
+++ README.txt
@@ -0,0 +1,12 @@
+Please double-click index.html to launch the map.
+
+The map is based on the 2022 Irish Census.
+Linguistic Data : https://www.cso.ie/en/media/csoie/census/census2022/SAPS_2022_Small_Area_UR_171024.csv
+Glossary : https://www.cso.ie/en/media/csoie/census/census2022/Glossary_Saps_2022_REVISED_21102024.xlsx
+Boundary : https://data.gov.ie/dataset/cso-small-areas-national-statistical-boundaries-2022-generalised-20m1/resource/afc77c17-d337-44cd-9fe7-aa4f5a9c2536
+
+2025-03-09 AM 6:37 
+
+Made by r/protonmap
+
+
---
+++ styles/English_Other_40s_5_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_English_Other_40s_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(168,187,215,0.7)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
